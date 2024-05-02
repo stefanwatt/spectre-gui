a74bbe66b9b5c4c2ea76ed35219c3edc4bd65cd3
Stefan Watt <stefan.watt@gmail.com>
2024-05-02 23:10:37 +0200

    refactored matches into singular

---
+++ frontend/src/Match.svelte
@@ -0,0 +1,20 @@
+<script>
+  /** @type {RipgrepMatch}*/
+  export let match;
+
+  /** @param {RipgrepMatch} match*/
+  function replace_match(match) {
+    console.log("replace_match", match);
+  }
+</script>
+
+<button
+  on:click={() => {
+    replace_match(match);
+  }}
+  class="cursor-pointer snap-start flex justify-start p-2"
+>
+  <code class="language-go w-4/5">
+    {match.MatchedLine}
+  </code>
+</button>
--- frontend/src/Results.svelte
+++ frontend/src/Results.svelte
@@ -5,7 +5,8 @@
   import ChevronUp from "./ChevronUp.svelte";
   import Matches from "./Matches.svelte";
   import ResultsHeader from "./ResultsHeader.svelte";
-  import { map_results } from "./results.service";
+  import { map_results, highlight_all } from "./results.service";
+  import Match from "./Match.svelte";
 
   /*** @type {RipgrepResultApi} */
   export let results;
@@ -13,35 +14,42 @@
   onMount(async () => {
     await import("./assets/prism.js");
     setTimeout(() => {
-      window.Prism.highlightAll(false);
+      highlight_all();
     });
   });
 
   $: mapped = map_results(results);
 
   $: {
-    if (mapped && window.Prism) {
-      window.Prism.highlightAll(false);
+    if (mapped) {
+      highlight_all();
     }
   }
 </script>
 
 {#if results && mapped}
-  {#each mapped as { path, matches }}
-    {#if !path.includes(":")}
-      <div class="flex pt-2 snap-start">
-        <div class="text-blue">
-          {#if !collapsed}
-            <ChevronDown></ChevronDown>
-          {:else}
-            <ChevronUp></ChevronUp>
-          {/if}
+  <div class="flex flex-col w-full">
+    {#each mapped as { path, matches }}
+      {#if !path.includes(":")}
+        <div class="grid grid-cols-[auto,1fr] pt-2 snap-start">
+          <div class="text-blue">
+            {#if !collapsed}
+              <ChevronDown></ChevronDown>
+            {:else}
+              <ChevronUp></ChevronUp>
+            {/if}
+          </div>
+          <div>
+            <ResultsHeader {path} match_count={matches.length}></ResultsHeader>
+
+            {#each matches as match}
+              {#if match.Path.length < 15 && match.Col.length < 5}
+                <Match {match}></Match>
+              {/if}
+            {/each}
+          </div>
         </div>
-        <div>
-          <ResultsHeader {path} match_count={matches.length}></ResultsHeader>
-          <Matches {matches}></Matches>
-        </div>
-      </div>
-    {/if}
-  {/each}
+      {/if}
+    {/each}
+  </div>
 {/if}
